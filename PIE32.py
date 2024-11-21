7204d259fa79606d51ea7b0a82dc99ccab18149b
Weisheng Zheng <nonagame888@gmail.com>
2024-11-22 00:19:08 +0100

    I'll work more later

--- PIE32.py
+++ PIE32.py
@@ -5,15 +5,20 @@ codigo_resultado = open("codigoResultado.txt", "w")
 
 nombreValido = r"[A-Za-z_][A-Za-z0-9_]*"
 
+finalizado = r".*?(?=;)"
+
 try:
     codigo = codigo_fuente.read()
-    palabras = codigo.split()
+    palabras = codigo.split() #creo que va a ser mejor hacer todo con expresiones regulares en vez de separar palabras
     for i in range(0, len(palabras)):
         if palabras[i] == "int":
             if re.match(nombreValido, palabras[i+1]):
                 linea = f"{palabras[i+1]}: word."
                 if palabras[i+2] == "=":
-                    linea += f" {palabras[i+3]}"
+                    n = 3;
+                    while (not(re.match(finalizado, palabras[i+n]))):
+                        linea += f" {palabras[i+n]}"
+                        n += 1
 
 
     print(linea)
