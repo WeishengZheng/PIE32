65332fc6db7d73d49bf22b789d4c11eb3bd23ce6
Weisheng Zheng <nonagame888@gmail.com>
2024-11-22 02:37:32 +0100

    I don't know what should I write

--- PIE32.py
+++ PIE32.py
@@ -9,6 +9,7 @@ finalizado = r".*?(?=;)"
 
 try:
     codigo = codigo_fuente.read()
+    print(len(codigo))
     palabras = codigo.split() #creo que va a ser mejor hacer todo con expresiones regulares en vez de separar palabras
     for i in range(0, len(palabras)):
         if palabras[i] == "int":
@@ -16,10 +17,9 @@ try:
                 linea = f"{palabras[i+1]}: word."
                 if palabras[i+2] == "=":
                     n = 3;
-                    while (not(re.match(finalizado, palabras[i+n]))):
-                        linea += f" {palabras[i+n]}"
-                        n += 1
-
+                    #while (not(re.match(finalizado, palabras[i+n]))):
+                        #linea += f" {palabras[i+n]}"
+                        #n += 1
 
     print(linea)
 finally:
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PIE32
 Developing a high-level programming language for MIPS32 assembly in Python, leveraging basic automata theory without prior knowledge of compilers.
+
+# Notes
+The lenguage do not support spaces before ";".
